472415e7b1fa7f66da2ab64d8bd379bc135c087c
Shai Shiff <shai@crossrider.com>
2016-05-16 14:49:55 +0300

    - start

--- sentences.js
+++ sentences.js
@@ -1,40 +1,7 @@
 
-var game = "(game|match)";
-var live = "(live|ongoing|current|curent)";
-var group = "(group|grup|groop|standing|table)";
-var today = "(today|todays|tonight|tonights|this evening|this evenings)";
-
-var teams_array = [
-  "Albania",
-  "Austria",
-  "Belgium",
-  "Croatia",
-  "Czech Republic",
-  "England",
-  "France",
-  "Germany",
-  "Hungary",
-  "Iceland",
-  "Italy",
-  "Northern Ireland",
-  "Poland",
-  "Portugal",
-  "Republic of Ireland",
-  "Romania",
-  "Russia",
-  "Slovakia",
-  "Spain",
-  "Sweden",
-  "Switzerland",
-  "Turkey",
-  "Ukraine",
-  "Wales"
-];
-
 var sentences = {
-  teams: teams_array,
-  page_welcome_msg: "Hey ! :)\nLet me know what kind of info you are looking for about Euro2016.\nTo get things started, you can write something like:\nShow me the groups\nOr even just write:\ngroups\n(if you're a bit lazy...)\nAnd last thing - just write:\nhelp\nto get some more info from me.\nHave fun !",
-  help_message: "Don't you worry ! I'll try and help out.\nI can show you a few cool things.\nTo see the groups just type something like:\ngroups\nor\nshow me the group\nHmm...and test's pretty much it for now. But don't worry I'll think about more things soon !",
+  page_welcome_msg: "Hey !\n Welcome to my shop !\nFeel free to look around :)\nDo you want me to show you what we have in stock ?",
+  help_message: "",
   user_welcoming_messages: [
     "^Hello",
     "^Hi",
@@ -55,14 +22,13 @@ var sentences = {
     "^Look what the cat dragged in"
   ],
   bot_welcoming_messages: [
-    "Hello :)",
-    "Hi there !",
-    "Hey !",
-    "Howdy...",
-    "Well hello.",
-    "Why hello there.",
-    "Yo !",
-    "Greetings."
+    "Hello",
+    "Hi there",
+    "Hey",
+    "Howdy",
+    "Well hello",
+    "Hello there",
+    "Greetings"
   ],
   user_says_thanks: [
     "^thanks$",
@@ -80,61 +46,7 @@ var sentences = {
     "No worries ;)",
     "Sure thing !",
     "You're welcome :)",
-    "I'm Here to help.",
-    "Don't worry about it."
-  ],
-  show_groups: [
-    "^Can you please show me the group",
-    "^Show me the group",
-    "^Show me group",
-    "^Show group",
-    "^What are the group",
-    "^Groups$",
-    "^Groops$",
-    "^Grops$",
-    "^Grups$",
-    "^Standings$",
-    "^Show standings$",
-    "^Show standing$",
-    "^Please show groups",
-    "^Please show me the groups",
-    "^Show groops",
-    "^Show me grups",
-    "^Show grups"
-  ],
-  show_team_games: [
-    "(.*)(" + teams_array.join("|") + ")(.*)" + game
-  ],
-  show_games_for_team: [
-    "(.*)" + game + "(.*)(" + teams_array.join("|") + ")(.*)"
-  ],
-  show_team_group: [
-    "(.*)(" + teams_array.join("|") + ")(.*)" + group
-  ],
-  show_group_for_team: [
-    "(.*)" + group + "(.*)(" + teams_array.join("|") + ")(.*)"
-  ],
-  show_live_games: [
-    "^show me " + live + " " + game,
-    "^" + live + " " + game,
-    "^show(.*)" + live + " " + game,
-    "^what(.*)" + live + " " + game,
-  ],
-  show_games_today: [
-    "^show me " + today + " " + game,
-    "^" + today + " " + game,
-    "^show(.*)" + today + " " + game,
-    "^what(.*)" + today + " " + game,
-  ],
-  help_me: [
-    "^help$",
-    "^help !",
-    "^help me",
-    "^hellp",
-    "^heelp",
-    "^helpp",
-    "^please help",
-    "^help please"
+    "I'm Here to help."
   ],
   bot_not_sure_what_user_means: [
     "Oopsy oops...not sure what you mean by that :(",
--- server.js
+++ server.js
@@ -1,17 +1,5 @@
 "use strict";
 
-// TODOs:
-// If sentence is unknown send to special conversation.
-// show team group
-// show team games
-// show live games
-// show group A|B|C
-// show next games
-// show quarter/semi/final game
-// show team stats/info
-// save user data to mongo as cache
-// Notifications about game events
-
 var Botkit = require('botkit');
 var Sentences = require('./sentences');
 var Api = require('./mockApi');
@@ -42,8 +30,10 @@ controller.middleware.receive.use(function(bot, message, next) {
   Utils.getUserInfo(message.user, function(userInfo) {
     if (userInfo) {
       message.userInfo = userInfo;
+      message.userFullName = userInfo.first_name + " " + userInfo.last_name;
       message.fullNameWithId = userInfo.first_name + "_" + userInfo.last_name + "_" + message.user;
     } else {
+      message.userFullName = "";
       message.fullNameWithId = message.user;
     }
     Utils.sendUserMsgToAnalytics(message.fullNameWithId, message.text);
@@ -74,111 +64,12 @@ controller.on('facebook_optin', function(bot, message) {
 
 // User said hello.
 controller.hears(Sentences.user_welcoming_messages, 'message_received', function(bot, message) {
-  bot.reply(message, Utils.randomFromArray(Sentences.bot_welcoming_messages));
+  bot.reply(message, Utils.randomFromArray(Sentences.bot_welcoming_messages) + " " + message.userFullName);
 });
 
 // User said thanks.
 controller.hears(Sentences.user_says_thanks, 'message_received', function(bot, message) {
   bot.reply(message, Utils.randomFromArray(Sentences.bot_says_you_are_welcome));
-});
-
-// User wants help.
-controller.hears(Sentences.help_me, 'message_received', function(bot, message) {
-  bot.reply(message, Sentences.help_message);
-});
-
-// User wants help.
-controller.hears(["אאא","אבג"] , 'message_received', function(bot, message) {
-  bot.reply(message, "הי !");
-});
-
-// Show the groups to the user.
-controller.hears(Sentences.show_groups, 'message_received', function(bot, message) {
-  Utils.showGroupsToUser(bot, message);
-});
-
-// Show the games of a specific team to the user.
-controller.hears(Sentences.show_team_games, 'message_received', function(bot, message) {
-  var team = null;
-  if (message.match.length > 2) {
-    team = message.match[2];  
-  }
-  if (typeof team === "string" && team.length > 0) {
-    console.log("Show games for " + team);
-    //bot.reply(message, "You want to see games for " + team + " ?");
-    Utils.showGamesToUser(bot, message, Api.getGamesOfTeam, team);
-  } else {
-    notSureWhatUserWants(bot, message);
-  }
-});
-
-// Show the games of a specific team to the user #2.
-controller.hears(Sentences.show_games_for_team, 'message_received', function(bot, message) {
-  var team = null;
-  if (message.match.length > 4) {
-    team = message.match[4];  
-  }
-  if (typeof team === "string" && team.length > 0) {
-    console.log("Show games for " + team);
-    //bot.reply(message, "You want to see games for " + team + " ?");
-    Utils.showGamesToUser(bot, message, Api.getGamesOfTeam, team);
-  } else {
-    notSureWhatUserWants(bot, message);
-  }
-});
-
-
-// Show the group of a specific team to the user.
-controller.hears(Sentences.show_team_group, 'message_received', function(bot, message) {
-  var team = null;
-  if (message.match.length > 2) {
-    team = message.match[2];  
-  }
-  if (typeof team === "string" && team.length > 0) {
-    console.log("Show group for " + team);
-    //bot.reply(message, "You want to see games for " + team + " ?");
-    Utils.showGroupsToUser(bot, message, Api.getGroupOfTeam, team);
-  } else {
-    notSureWhatUserWants(bot, message);
-  }
-});
-
-// Show the games of a specific team to the user #2.
-controller.hears(Sentences.show_group_for_team, 'message_received', function(bot, message) {
-  var team = null;
-  if (message.match.length > 4) {
-    team = message.match[4];  
-  }
-  if (typeof team === "string" && team.length > 0) {
-    console.log("Show group for " + team);
-    //bot.reply(message, "You want to see games for " + team + " ?");
-    Utils.showGroupsToUser(bot, message, Api.getGroupOfTeam, team);
-  } else {
-    notSureWhatUserWants(bot, message);
-  }
-});
-
-// Show live games.
-controller.hears(Sentences.show_live_games, 'message_received', function(bot, message) {
-  console.log("Show live games to user");
-  Utils.showGamesToUser(bot, message, Api.getLiveGames);
-});
-
-// Show todays games.
-controller.hears(Sentences.show_games_today, 'message_received', function(bot, message) {
-  console.log("Show todays games to user");
-  Utils.showGamesToUser(bot, message, Api.getGamesByDate, DateFormat(new Date(), "dd/mm/yy"));
-});
-
-// Test
-controller.hears(['test'], 'message_received', function(bot, message) {
-  bot.startConversation(message, function(err, convo) {
-     convo.say('Ok...');
-     convo.ask('What are you testing ?', function(response, convo) {
-       convo.say('yeah, let\'s test ' + response.text);
-       convo.next();
-    });
-  });
 });
 
 // Not suer what the users wants. Final fallback.
@@ -189,7 +80,7 @@ controller.on('message_received', function(bot, message) {
     console.log("Found intent: " + matchedIntent);
     bot.reply(message, "Did you mean " + matchedIntent + " ?");
   } else {
-    notSureWhatUserWants(bot, message);    
+    notSureWhatUserWants(bot, message);
   }
   return false;
 });
@@ -203,15 +94,6 @@ function notSureWhatUserWants(bot, message) {
 // Facebook postsbacks.
 controller.on('facebook_postback', function(bot, message) {
   Utils.sendToAnalytics(message.user, "facebook_postback-" + message.payload, "incoming");
-  if (message.payload.indexOf('show_games_for_') === 0) {
-    var teamName = message.payload.replace("show_games_for_","");
-    bot.reply(message, 'Games for ' + teamName);
-    Utils.showGamesToUser(bot, message, Api.getGames);
-  } else if (message.payload.indexOf('set_notifications_for_team_') === 0) {
-    var team = message.payload.replace("set_notifications_for_team_","");
-    bot.reply(message, "Sure thing ! You will get notifications for " + team + " from now on.");
-  } else if (message.payload.indexOf('set_notifications_for_game_') === 0) {
-    var gameId = message.payload.replace("set_notifications_for_game_","");
-    bot.reply(message, "Sure thing ! You will get notifications for this game from now on.");
+  if (message.payload.indexOf('XXX') === 0) {
   }
 });
