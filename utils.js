2599760d0e035bc7ca86fc57a8cad82e32e71b24
Shai Shiff <shai@crossrider.com>
2016-05-17 15:19:31 +0300

    - fix

--- utils.js
+++ utils.js
@@ -235,7 +235,7 @@ function addToUserCartInternal(userId, prod_id, prod_name, prod_price, callback)
       prod_price: prod_price
     });
     removeUserInfoFromMongo(userId, Consts.MONGODB_USER_CART_COL, function() {
-      insertUserInfoToMongo(userId, Consts.MONGODB_USER_CART_COL, function() {
+      insertUserInfoToMongo(userCart, Consts.MONGODB_USER_CART_COL, function() {
         callback(userCart)
       });
     });
